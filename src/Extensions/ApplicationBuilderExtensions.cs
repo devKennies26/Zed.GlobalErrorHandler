97c8177c40a308ca127c940e1c9a5014703a0af5
zaminalili <zaminalili29@gmail.com>
2025-07-16 12:05:29 +0400

    Add extensions methods for register package

---
+++ src/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Zed.GlobalErrorHandler.Middleware;
+
+namespace Zed.GlobalErrorHandler.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseGlobalErrorHandler(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<ErrorHandlingMiddleware>();
+    }
+}
---
+++ src/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Zed.GlobalErrorHandler.Mappers;
+using Zed.GlobalErrorHandler.Middleware;
+
+namespace Zed.GlobalErrorHandler.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddGlobalErrorHandler(this IServiceCollection services, 
+        Action<Dictionary<Type, int>> configure, 
+        int defaultStatusCode = StatusCodes.Status500InternalServerError, 
+        string defaultErrorMessage = "Something went wrong.")
+    {
+        var mapping = new Dictionary<Type, int>();
+        configure.Invoke(mapping);
+        
+        services.AddSingleton<ExceptionToStatusCodeMapper>(new ExceptionToStatusCodeMapper(mapping, 
+            defaultStatusCode, defaultErrorMessage));
+        services.AddTransient<ErrorHandlingMiddleware>();
+        
+        return services;
+    }
+}
