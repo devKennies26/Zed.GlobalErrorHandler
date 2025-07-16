d309dd2f6cc550ba1282913036a6dbbfd81fcf61
zaminalili <zaminalili29@gmail.com>
2025-07-16 11:23:14 +0400

    Add ExceptionToStatusCodeMapper

---
+++ src/Mappers/ExceptionToStatusCodeMapper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Zed.GlobalErrorHandler.Mappers;
+
+public class ExceptionToStatusCodeMapper
+{
+    private readonly Dictionary<Type, int> exceptionStatusCodes;
+    public int DefaultStatusCode { get; private set; }
+    public string DefaultErrorMessage { get; private set; }
+
+    public ExceptionToStatusCodeMapper(Dictionary<Type, int> exceptionStatusCodes, int defaultStatusCode, string defaultErrorMessage)
+    {
+        this.exceptionStatusCodes = exceptionStatusCodes;
+        DefaultStatusCode = defaultStatusCode;
+        DefaultErrorMessage = defaultErrorMessage;
+    }
+
+    public int Map(Exception exception)
+    {
+        return exceptionStatusCodes.TryGetValue(exception.GetType(), out var code)
+            ? code
+            : DefaultStatusCode;
+    }
+}
